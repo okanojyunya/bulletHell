e7d1b8b0ab57b7c6ada53d4b8aaa8dccb27e0b3e
okanojyunya <monogamingvantan@gmail.com>
2024-06-25 11:42:46 +0900

    HPvalueの実装

--- Assets/Scripts/EnemyHP.cs
+++ Assets/Scripts/EnemyHP.cs
@@ -2,22 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.U2D;
+using UnityEngine.UI;
 //敵のHPのスクリプト
 public class EnemyHP : MonoBehaviour
 {
+    public Slider HP;
     /// <summary>敵のHPの変数</summary>
-    [SerializeField] float m_enemyHp = 0;
+    float m_enemyHp = 10;
+
+    float Hp;
+
+    private void Start()
+    {
+        HP.value = 10;
+        Hp = m_enemyHp;
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag =="Bullet")
         {
-            m_enemyHp -= 1;
+
+            //m_enemyHp -= 1;
+            Hp -= 1;
+            HP.value = (float)Hp;
+           
         }
 
-        if (m_enemyHp == 0)
+        if (Hp == 0)
         {
             Destroy(this.gameObject);
+            Destroy(HP);
         }
 
     }
